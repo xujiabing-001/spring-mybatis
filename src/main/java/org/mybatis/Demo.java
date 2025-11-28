339a196a868950b1e7b60637841633782a2b3eb4
xujiabing <jiabing.xu@speediance.com>
2025-11-28 09:56:53 +0800

    我要提交啦

---
+++ src/main/java/org/mybatis/Demo.java
@@ -0,0 +1,37 @@
+package org.mybatis;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static java.lang.System.in;
+
+/**
+ * @author xujiabing
+ * @email jiabing.xu@speediance.com
+ * @date 2025/11/28 9:29
+ * @Copyright Copyright (c)  Speediance Inc. All Rights Reserved.
+ */
+public class Demo {
+
+  public static void main(String[] args) {
+    m1();
+
+    m2();
+  }
+
+  private static int m1() {
+
+    int i = 3 / 0;
+
+    return i;
+  }
+
+  private static void m2() {
+
+    String A = "11111";
+
+    InputStream inputStream = new FileInputStream("/aa/bb");
+
+  }
+
+}
